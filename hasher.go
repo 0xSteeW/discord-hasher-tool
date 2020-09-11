124d3fe75b0f796f0f75e3d8370f79c2a81d7fd8
Daniel Pérez <steew0x8@protonmail.com>
2020-09-11 18:54:02 +0200

    Added image cropper

--- hasher.go
+++ hasher.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"os"
-	"time"
-	"net/http"
-	"os/signal"
-	"io/ioutil"
-	"strings"
-	"log"
-	"syscall"
 	"image"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/corona10/goimagehash"
-	"bufio"
+	"github.com/oliamb/cutter"
 	"gopkg.in/yaml.v2"
 )
 
@@ -29,7 +31,7 @@ type Hashes struct {
 func main() {
 	fmt.Println("Welcome to the hash updater. To update the hashes type in any channel with the desired pokemon bot: hasher-update")
 	client, err := discordgo.New("token")
-	if (err != nil) {
+	if err != nil {
 		fmt.Printf("Invalid token or session error: %s", err)
 		os.Exit(1)
 	}
@@ -49,7 +51,7 @@ func main() {
 }
 
 func messageCreate(client *discordgo.Session, message *discordgo.MessageCreate) {
-	if (!message.Author.Bot && strings.HasPrefix(message.Content, "hasher-")) {
+	if !message.Author.Bot && strings.HasPrefix(message.Content, "hasher-") {
 		command := strings.TrimPrefix(message.Content, "hasher-")
 		switch command {
 		case "update":
@@ -62,7 +64,7 @@ func messageCreate(client *discordgo.Session, message *discordgo.MessageCreate) 
 		fmt.Printf("Received image for pokemon: %s\n", currentPokemon)
 		url := message.Embeds[0].Image.URL
 		imageDecoder := Download(url)
-		hash := Hash(imageDecoder)
+		hash := Hash(CropUselessArea(imageDecoder))
 		pokemonReceived <- hash
 	}
 }
@@ -101,11 +103,11 @@ func HashUpdater(client *discordgo.Session, channelID string) {
 	hashes.Hashes = make(map[string]string)
 	pokemonNames = ReadNames()
 	for idx, name := range pokemonNames {
-		time.Sleep(3*time.Second)
+		time.Sleep(3 * time.Second)
 		currentPokemon = name
 		fmt.Printf("Hasing now: %s, %d/%d", name, idx, len(pokemonNames))
 		client.ChannelMessageSend(channelID, fmt.Sprintf("p!info %s", name))
-		hash := <- pokemonReceived
+		hash := <-pokemonReceived
 		hashes.Hashes[name] = hash
 	}
 	// For loop finished, marshal the hashes into YAML format
@@ -115,6 +117,126 @@ func HashUpdater(client *discordgo.Session, channelID string) {
 		os.Exit(1)
 	}
 	WriteList(content)
+}
+func CropUselessArea(img *image.Image) image.Image {
+	topLeft, bottomRight := FindVisibleVertexes(*img)
+	size := image.Point{X: bottomRight.X - topLeft.X, Y: bottomRight.Y - topLeft.Y}
+	fmt.Println(size)
+	newImg, _ := cutter.Crop(*img, cutter.Config{
+		Width:  size.X,
+		Height: size.Y,
+		Anchor: topLeft,
+		Mode:   cutter.TopLeft,
+	})
+	return newImg
+}
+
+func FindVisibleVertexes(img image.Image) (image.Point, image.Point) {
+	var COLOR_TRESHOLD int8 = 50
+	// Iterate over img.At(), because it gives a color.Color object. Test if that color.Color is not empty, and seek for the nearest to each border.
+	sizeX := img.Bounds().Max.X
+	sizeY := img.Bounds().Max.Y
+	// First get top left vertex, starting from left border
+	fmt.Printf("Size: %d,%d\n", sizeX, sizeY)
+	var currentLowest int
+	var currentVertex image.Point
+	var topLeft image.Point
+	var bottomRight image.Point
+	// sizeX < sizeY ? sizeY+1 : sizeX+1 , assign whichever is higer, and to the max size of the image, so no value can be higher than currentLowest
+	if sizeX < sizeY {
+		currentLowest = sizeY + 1
+	} else {
+		currentLowest = sizeX + 1
+	}
+
+	// Left border
+	for row := 0; row < sizeY; row++ {
+		for pixel := 0; pixel < sizeX; pixel++ {
+			c := img.At(pixel, row)
+			_, _, _, alpha := c.RGBA()
+			if int8(alpha) > COLOR_TRESHOLD {
+				// Found non-transparent pixel, check if the distance from lowest is less
+				if pixel < currentLowest {
+					currentLowest = pixel
+					currentVertex = image.Point{X: pixel, Y: row}
+				}
+				// Break current column after having found non-transparent pixel
+			}
+
+		}
+	}
+	topLeft.X = currentVertex.X
+
+	if sizeX < sizeY {
+		currentLowest = sizeY + 1
+	} else {
+		currentLowest = sizeX + 1
+	}
+	currentVertex = image.Point{0, 0}
+	// Top border
+	for column := 0; column < sizeX; column++ {
+		for pixel := 0; pixel < sizeY; pixel++ {
+			c := img.At(column, pixel)
+			_, _, _, alpha := c.RGBA()
+			if int8(alpha) > COLOR_TRESHOLD {
+				// Found non-transparent pixel, check if the distance from lowest is less
+				if pixel < currentLowest {
+					currentLowest = pixel
+					currentVertex = image.Point{X: column, Y: pixel}
+				}
+			}
+
+		}
+	}
+	topLeft.Y = currentVertex.Y
+	if sizeX < sizeY {
+		currentLowest = sizeY + 1
+	} else {
+		currentLowest = sizeX + 1
+	}
+	// Right
+	for row := 0; row < sizeY; row++ {
+		// Just change the pixel direction (y stays)
+		for pixel := sizeX - 1; pixel >= 0; pixel-- {
+			c := img.At(pixel, row)
+			_, _, _, alpha := c.RGBA()
+			if int8(alpha) > COLOR_TRESHOLD {
+				// Found non-transparent pixel, check if the distance from lowest is less
+				if pixel < currentLowest {
+					currentLowest = pixel
+					currentVertex = image.Point{X: sizeX - 1 - pixel, Y: row}
+				}
+				// Break current column after having found non-transparent pixel
+			}
+
+		}
+	}
+	bottomRight.X = currentVertex.X
+	if sizeX < sizeY {
+		currentLowest = sizeY + 1
+	} else {
+		currentLowest = sizeX + 1
+	}
+	currentVertex = image.Point{0, 0}
+
+	// Bottom
+	for column := 0; column < sizeX; column++ {
+		for pixel := sizeY - 1; pixel >= 0; pixel-- {
+			c := img.At(column, pixel)
+			_, _, _, alpha := c.RGBA()
+			if int8(alpha) > COLOR_TRESHOLD {
+				// Found non-transparent pixel, check if the distance from lowest is less
+				if pixel < currentLowest {
+					currentLowest = pixel
+					currentVertex = image.Point{X: column, Y: sizeY - 1 - pixel}
+				}
+			}
+
+		}
+	}
+	bottomRight.Y = currentVertex.Y
+
+	return topLeft, bottomRight
 }
 
 func Download(url string) *image.Image {
@@ -129,8 +251,8 @@ func Download(url string) *image.Image {
 	return &decoded
 }
 
-func Hash(imageDecoder *image.Image) string {
-	hash, err := goimagehash.PerceptionHash(*imageDecoder)
+func Hash(imageDecoder image.Image) string {
+	hash, err := goimagehash.PerceptionHash(imageDecoder)
 	if err != nil {
 		log.Panic("Could not get the hash of ", currentPokemon)
 	}
